10c5731b5c65f8849707101a1ec431aef775f75f
Marc Willemsen <marc@bolt.nl>
2019-02-08 18:53:57 +0100

    extra secure

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 //jshint esversion:6
 require ('dotenv').config();
+// const md5 = require('md5');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -27,7 +31,7 @@ const userSchema = new mongoose.Schema ( {
 
 const secret = process.env.SECRET;
 
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("user", userSchema);
 
@@ -46,13 +50,15 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
+
+bcrypt.hash(req.body.password, saltRounds, function(err,hash){
+
   const newUser = new User({
 
     email: req.body.username,
-    password: req.body.password
+    password: hash
 
   });
-
   newUser.save(function(err) {
     if (err) {
       console.log(err);
@@ -61,6 +67,10 @@ app.post("/register", function(req, res) {
     }
   });
 });
+});
+
+
+
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
@@ -73,13 +83,14 @@ app.post("/login", function(req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        } else {
-          res.redirect("/login");
+
+        bcrypt.compare(password, foundUser.password, function(err, result){
+if (result === true){
+res.render("secrets");
+}
+        });
         }
       }
-    }
   });
 });
 
